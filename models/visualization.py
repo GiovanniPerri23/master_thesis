e5b68466ca443733932c7b30bdb9c7793f4a29ac
Giovanni Perri <giovanni.jperri23@gmail.com>
2024-02-12 11:39:52 +0000

    mean error

--- models/visualization.py
+++ models/visualization.py
@@ -42,12 +42,11 @@ def plot_error_metrics_barchart(error_table):
     ax1.set_title('NMAE for Each Column')
 
     # Grafico a barre per MAE, RMSE, MAPE
-    ax2.bar(error_table['Column'] - 0.2, error_table['MAE'], width=0.2, color='green', label='MAE', alpha=0.7)
     ax2.bar(error_table['Column'], error_table['RMSE'], width=0.2, color='orange', label='RMSE', alpha=0.7)
     ax2.bar(error_table['Column'] + 0.2, error_table['MAPE'], width=0.2, color='red', label='MAPE', alpha=0.7)
     ax2.set_xlabel('Columns')
     ax2.set_ylabel('Errors')
-    ax2.set_title('MAE, RMSE and MAPE for Each Column')
+    ax2.set_title('RMSE and MAPE for Each Column')
     ax2.legend()
 
     # Trova il minimo di NMAE, RMSE e MAPE
@@ -72,45 +71,6 @@ def plot_error_metrics_barchart(error_table):
     plt.tight_layout()
     plt.show()
 
-    """
-    Plots a bar chart to visualize error metrics for each column.
-
-    Parameters:
-    error_table (DataFrame): A DataFrame containing error metrics for each column.
-
-    Returns:
-    None
-    """
-    # Create subplots
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 4))
-
-    # Grafico a barre per NMAE
-    ax1.bar(error_table['Column'], error_table['NMAE'], color='blue', alpha=0.7)
-    ax1.set_xlabel('Columns')
-    ax1.set_ylabel('NMAE')
-    ax1.set_title('NMAE for Each Column')
-
-    # Grafico a barre per MAE, RMSE, MAPE
-    # ax2.bar(error_table['Column'] - 0.2, error_table['MAE'], width=0.2, color='green', label='MAE', alpha=0.7)
-    ax2.bar(error_table['Column'], error_table['RMSE'], width=0.2, color='orange', label='RMSE', alpha=0.7)
-    ax2.bar(error_table['Column'] + 0.2, error_table['MAPE'], width=0.2, color='red', label='MAPE', alpha=0.7)
-    ax2.set_xlabel('Columns')
-    ax2.set_ylabel('Errors')
-    ax2.set_title('RMSE and MAPE for Each Column')
-    ax2.legend()
-
-    # Trova il minimo di NMAE, RMSE e MAPE
-    min_nmae_row = error_table.loc[error_table['NMAE'].idxmin()]
-    min_rmse_row = error_table.loc[error_table['RMSE'].idxmin()]
-    min_mape_row = error_table.loc[error_table['MAPE'].idxmin()]
-
-    # Stampa i minimi e le relative colonne
-    print("Min NMAE:", min_nmae_row['NMAE'], "for Column:", min_nmae_row['Column'])
-    print("Min RMSE:", min_rmse_row['RMSE'], "for Column:", min_rmse_row['Column'])
-    print("Min MAPE:", min_mape_row['MAPE'], "for Column:", min_mape_row['Column'])
-    print((min_nmae_row['NMAE'], min_nmae_row['RMSE'], min_nmae_row['MAPE']))
-    plt.tight_layout()
-    plt.show()
     
 def plot_model_rmse_and_loss(history):
     
