23e513f19fbdcdb651dd19789f374bff1da72dd4
Giovanni Perri <giovanni.jperri23@gmail.com>
2024-02-19 15:01:11 +0000

    adding data and preprocessing codes

--- data/creating_data.py
+++ data/creating_data.py
@@ -4,6 +4,17 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 
 def create_dataframe_from_xml(xml_folder, root_findall):
+    """
+    Create a pandas DataFrame from XML files in a given folder.
+
+    Args:
+        xml_folder (str): Path to the folder containing XML files.
+        root_findall (str): XPath expression to find the root elements in the XML files.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing the data extracted from the XML files.
+    """
+
     df = pd.DataFrame() 
 
     for filename in os.listdir(xml_folder):
@@ -30,10 +41,12 @@ def create_dataframe_from_xml(xml_folder, root_findall):
             # Aggiungi il DataFrame del file corrente al DataFrame principale
             df = pd.concat([df, temp_df], ignore_index=True)
 
-
     return df
-# Conta i valori NaN nel dataframe
+
 def nan_counts_func(df):
+    """
+    Calculates the number of NaN values in each column of a DataFrame.
+    """
     nan_counts = df.isna().sum()
 
     # Trova le colonne con valori NaN (se ce ne sono)
@@ -44,9 +57,26 @@ def nan_counts_func(df):
     else:
         print("Colonne con valori NaN:")
         print(columns_with_nan)
-    
-df_price = create_dataframe_from_xml("data_0", './/Prezzi')
-df_stime = create_dataframe_from_xml("data_0", './/marketintervaldetail')
-df_gas = create_dataframe_from_xml('data_1', './/negoziazione_continua')
-df_demand = create_dataframe_from_xml("data_1", './/Fabbisogno')
-df_price2 = create_dataframe_from_xml("data_old", './/Prezzi')
+
+if __name__ == "__main__":
+    df_price = create_dataframe_from_xml("prices", './/Prezzi')
+    df_stime = create_dataframe_from_xml("stimafabbisogno", './/marketintervaldetail')
+    df_gas = create_dataframe_from_xml('gas', './/negoziazione_continua')
+    df_demand = create_dataframe_from_xml("fabbisogno", './/Fabbisogno')
+
+    # Preprocessing GAS data
+    df_gas = df_gas.groupby('NomeProdotto')['PrezzoMedio'].last().reset_index()
+    df_gas.columns = ['NomeProdotto', 'GAS']
+    df_gas['NomeProdotto'] = df_gas['NomeProdotto'].str.replace('MGP-', '').str.replace('WE-', '')
+
+    # To FIX: bisogna rimuovere i valori WE- dal dataset, per ora li rimuovo manualmente
+    df_gas = df_gas.iloc[:2176]
+    # df_gas = df_gas.iloc[:31,]
+    df_gas['DataG'] = pd.to_datetime(df_gas['NomeProdotto'])
+    df_gas = df_gas[['DataG', 'GAS']]
+    df_gas['GAS'] = pd.to_numeric(df_gas['GAS'], errors='coerce')
+
+    df_gas = df_gas.loc[df_gas.index.repeat(24)].reset_index(drop=True)
+
+    df = pd.concat([df_price['Data'], df_price['Ora'], df_price['PUN'], df_stime['Totale'], df_gas, df_demand['Italia']], axis=1)
+    df.to_csv("data_to_process.csv", index=True)
